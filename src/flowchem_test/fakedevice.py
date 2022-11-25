8a04f70b6b36fd0105868c773b3329e5e36ac790
Dario Cambie <dario.cambie@mpikg.mpg.de>
2022-11-25 11:44:54 +0100

    Move test component from flowchem to flowchem-test

--- src/flowchem_test/fakedevice.py
+++ src/flowchem_test/fakedevice.py
@@ -2,10 +2,11 @@
 from collections.abc import Iterable
 
 from flowchem.components.base_component import FlowchemComponent
-from flowchem.components.test import TestComponent
 from flowchem.devices.flowchem_device import DeviceInfo
 from flowchem.devices.flowchem_device import FlowchemDevice
 from flowchem.people import dario
+
+from .test_component import TestComponent
 
 
 __all__ = ["FakeDevice"]
