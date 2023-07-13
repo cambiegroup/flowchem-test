3cbfb32b7455a3d088e1acb937f658d63b944d50
Dario Cambie <dario.cambie@mpikg.mpg.de>
2023-07-13 18:34:52 +0200

    0.1b3

--- src/flowchem_test/fakedevice.py
+++ src/flowchem_test/fakedevice.py
@@ -1,26 +1,28 @@
 """Fake device for testing purposes. No parameters needed."""
-from collections.abc import Iterable
-
 from flowchem.components.base_component import FlowchemComponent
 from flowchem.devices.flowchem_device import DeviceInfo
 from flowchem.devices.flowchem_device import FlowchemDevice
+
 from flowchem.utils.people import dario
 
-from .test_component import TestComponent
+
+class TestComponent(FlowchemComponent):
+    def __init__(self, name: str, hw_device: FlowchemDevice):
+        """Initialize a TestComponent with the provided endpoints."""
+        super().__init__(name, hw_device)
+        self.add_api_route("/test", lambda: True, methods=["GET"])
 
 
 class FakeDevice(FlowchemDevice):
     device_info = DeviceInfo(
-        authors=[dario],
-        maintainers=[dario],
-        manufacturer="virtual-device",
-        model="FakeDevice",
-        serial_number=42,
-        version="v1.0",
-    )
+            authors=[dario],
+            maintainers=[dario],
+            manufacturer="virtual-device",
+            model="FakeDevice",
+            serial_number=42,
+            version="v1.0",
+        )
 
-    def components(self) -> Iterable[FlowchemComponent]:
-        """Returns a test Component."""
-        component = TestComponent(name="test-component", hw_device=self)
-        component.add_api_route("/test", lambda: True, methods=["GET"])
-        return (component,)
+    async def initialize(self):
+        """Add component to device."""
+        self.components.append(TestComponent(name="test-component", hw_device=self))
